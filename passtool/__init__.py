3b6abb842375edf76fcbb861ccc79743d1cafbde
Josh Bollinger <bollingerjk at gmail.com>
2025-02-17 15:36:33 -0500

    Updates for gunicorn and cleanup

--- passtool/__init__.py
+++ passtool/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_oidc import OpenIDConnect
 from flask_bootstrap import Bootstrap5
-from flask_htmx import HTMX
 import os
 from .config import Config, DevConfig
 
@@ -10,15 +9,17 @@ os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/certs/ca-certificates.crt'
 app = Flask(__name__)
 oidc = OpenIDConnect()
 bs = Bootstrap5()
-htmx = HTMX()
-config = DevConfig()
+
+if 'FLASK_DEV' in os.environ:
+    config = DevConfig()
+else:
+    config = Config()
 
 
 def create_app():
     app.config.from_object(config)
     oidc.init_app(app)
     bs.init_app(app)
-    htmx.init_app(app)
     from .views import views
     app.register_blueprint(views)
     return app
